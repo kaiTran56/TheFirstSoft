3a44fdcd943e2b0e94d1a6e81b2bce429da4d003
TranKai <quyettran057@gmail.com>
2020-09-12 08:40:43 +0700

    Book Object

---
+++ Book.java
@@ -0,0 +1,28 @@
+public class Book{
+	private String name ;
+	private String author;
+	private double price;
+	public Book(String name , String author, double price ){
+		this.name = name ;
+		this.author = author;
+		this.price = price;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public String getAuthor(){
+		return this.author;
+	}
+	public double getPrice(){
+		return this.price;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setAuthor(String author){
+		this.author = author;
+	}
+	public void setPrice(double pricce){
+		this.price = price;
+	}
+}
